56cc1c3a62ce96b2a403e333cc8574ae15f00af0
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-09-22 15:59:08 +0500

    Task2

---
+++ Tyuiu.ZavyalovKA.Sprint1.Task2.V4.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ZavyalovKA.Sprint1.Task2.V4.Lib;
+namespace Tyuiu.ZavyalovKA.Sprint1.Task2.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.CalculateSquare(x);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
---
+++ Tyuiu.ZavyalovKA.Sprint1.Task2.V4/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.ZavyalovKA.Sprint1.Task2.V4.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт #1 | Выполнил: Завьялов Константин Андреевич | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Арифмитические операторы в C#                                     *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #4                                                              *");
+Console.WriteLine("* Выполнил: Завьялов Константин Андреевич | РППб-25-1                     *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране              *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+int x;
+Console.WriteLine("Длина стороны квадрата: ");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Площадь квадрата = " + ds.CalculateSquare(x));
+Console.ReadLine();
+
