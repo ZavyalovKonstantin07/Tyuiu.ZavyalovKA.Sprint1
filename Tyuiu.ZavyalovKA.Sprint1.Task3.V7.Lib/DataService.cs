32ad0a8a8e30cfef13da1e06d6b28136c5d0cc57
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-09-22 19:31:48 +0500

    Task3

--- Tyuiu.ZavyalovKA.Sprint1.Task3.V7.Lib/DataService.cs
+++ Tyuiu.ZavyalovKA.Sprint1.Task3.V7.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.ZavyalovKA.Sprint1.Task3.V7.Lib
 {
-    public class DataService
+    public class DataService: ISprint1Task3V7
     {
         public double VerstsToKilometers(double versts)
         {
