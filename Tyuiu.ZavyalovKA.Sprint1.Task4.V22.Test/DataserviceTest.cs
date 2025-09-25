bc1170ba218cf95182ca337ef30bcabec7b86319
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-09-25 15:09:41 +0500

    Task 4

---
+++ Tyuiu.ZavyalovKA.Sprint1.Task4.V22.Test/DataserviceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.ZavyalovKA.Sprint1.Task4.V22.Lib;
+namespace Tyuiu.ZavyalovKA.Sprint1.Task4.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = 1;
+            double wait = 0.054;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res); 
+        }
+    }
+}
