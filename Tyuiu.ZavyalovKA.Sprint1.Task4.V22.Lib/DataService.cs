f3ab1a8d214608c65d1ab0e17fc7d32b3367f31c
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-09-25 15:01:16 +0500

    Task4

--- Tyuiu.ZavyalovKA.Sprint1.Task4.V22.Lib/DataService.cs
+++ Tyuiu.ZavyalovKA.Sprint1.Task4.V22.Lib/DataService.cs
@@ -5,8 +5,8 @@ namespace Tyuiu.ZavyalovKA.Sprint1.Task4.V22.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res = Math.Sqrt(x * y) / (1 + Math.Pow(x + 2 * y, 2));  
+            double res = Math.Sqrt(x * y) / (1 + Math.Pow(x + 2 * y, 2));
             return Math.Round(res, 3);
-        } 
+        }
     }
 }
