9de5043e9e7feac231156b6711a7545a86f690fa
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-09-25 18:53:46 +0500

    Task6

--- Tyuiu.ZavyalovKA.Sprint1.Task6.V9.Lib/DataService.cs
+++ Tyuiu.ZavyalovKA.Sprint1.Task6.V9.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint1;
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.ZavyalovKA.Sprint1.Task6.V9.Lib
 {
     public class DataService : ISprint1Task6V9
@@ -9,7 +10,33 @@ namespace Tyuiu.ZavyalovKA.Sprint1.Task6.V9.Lib
             {
                 return "";
             }
-            return value[^1] + value[..^1];
+
+            string[] words = value.Split(' ');
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(words[i]))
+                {
+                    string transformedWord = TransformWord(words[i]);
+                    result.Append(transformedWord);
+                }
+                if (i < words.Length - 1)
+                {
+                    result.Append(" ");
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private string TransformWord(string word)
+        {
+            if (word.Length <= 1)
+            {
+                return word;
+            }
+            return word[^1] + word[..^1];
         }
     }
 }
